ba49f5e9535e59da9445f1808fb5200e8202dff1
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2025-02-02 21:34:26 +0100

    Level Manager added
    ancora manca il sistema di Exp

---
+++ Assets/Scripts/Player/PlayerLevelHandler.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerLevelHandler : MonoBehaviour
+{
+    [SerializeField] private int level = 1;
+    public int Level => level;
+    [SerializeField] private float actualExp = 0f;
+    public float ActualExp => actualExp;
+    [SerializeField] private float grabExpRange = 1f;
+
+    
+    [SerializeField, Tooltip("trueLevelUpThreshold = levelUpThreshold * level * thresholdMultiplayer")]
+    private float trueLevelUpThreshold = 300f;
+    [SerializeField] private float levelUpThreshold = 300f;
+    [SerializeField] private float thresholdMultiplayer = 1f;
+
+    private CircleCollider2D _collider;
+    
+    void Awake()
+    {
+        _collider = GetComponent<CircleCollider2D>();
+    }
+
+    private void Start()
+    {
+        // EventManager.OnPlayerLevelUp += (level, manager) => { BarsUI.instance.SetExp((float)0, GetComponentInParent<PlayerInput>().playerIndex);
+        //                                                       BarsUI.instance.SetMaxExp((float)_trueLevelUpThreshold, GetComponentInParent<PlayerInput>().playerIndex);
+        // };
+        // BarsUI.instance.SetMaxExp((float)_trueLevelUpThreshold, GetComponentInParent<PlayerInput>().playerIndex);
+    }
+
+    void Update()
+    {
+        trueLevelUpThreshold = levelUpThreshold * level * thresholdMultiplayer;
+    }
+
+    public void AddExp(float expToAdd)
+    {
+        actualExp += expToAdd;
+
+        //BarsUI.instance.SetExp(_actualExp, GetComponentInParent<PlayerInput>().playerIndex);
+
+
+        if (actualExp >= trueLevelUpThreshold)
+        {
+            level++;
+            actualExp = 0f;
+
+            //if (EventManager.OnPlayerLevelUp != null)
+            //{
+            //    Debug.Log($"Registered methods: {EventManager.OnPlayerLevelUp.GetInvocationList().Length}");
+            //}
+            //EventManager.OnPlayerLevelUp?.Invoke(_level, this);
+
+            //SI lo so che � sbagliato perch� c'� l'evento sopra
+            //GameHUDmanager.instance.UpdateLvlText(this, _level);
+        }
+    }
+
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if(other.gameObject.TryGetComponent(out ExpItem expItem))
+    //     {
+    //         AddExp(expItem.EXPValue);
+    //         Destroy(expItem.gameObject);
+    //     }
+    // }
+
+    public void ChangeGrabRange()
+    {
+        _collider.radius = grabExpRange;
+    }
+}
