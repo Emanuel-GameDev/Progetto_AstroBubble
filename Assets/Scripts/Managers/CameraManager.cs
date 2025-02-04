3eb4e8119107b0e43ea097c73271faca6adba132
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2025-02-04 12:46:32 +0100

    no era l'altro

---
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField]
+    private CinemachineTargetGroup targetGroup;
+    
+    private void Start()
+    {
+        if (targetGroup == null) return;
+        
+        List<PlayerInputHandler> inputs = PlayerConfigurationManager.Instance.PlayerInputHandlers; 
+        Debug.Log(inputs.Count);
+
+        for (int i = 0; i < inputs.Count; i++)
+        {
+            targetGroup.AddMember(inputs[i].gameObject.transform, 1f, 1f);
+        }
+    }
+}
