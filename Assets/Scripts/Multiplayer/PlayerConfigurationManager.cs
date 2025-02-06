7d0c601f1874072995a8341125c12a42f348ae09
Emanuel-Creator <120394201+Emanuel-GameDev@users.noreply.github.com>
2025-02-07 00:50:48 +0100

    Color Choice

--- Assets/Scripts/Multiplayer/PlayerConfigurationManager.cs
+++ Assets/Scripts/Multiplayer/PlayerConfigurationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -14,14 +15,18 @@ public class PlayerConfigurationManager : MonoBehaviour
     [SerializeField]
     private int maxPlayers = 2;
     public int MaxPlayers => maxPlayers;
+    
+    
+    [SerializeField] private List<Color> _playerColors = new List<Color>();
+    
+    private List<PlayerDefaultSetting> _defaultPlayers;
+    public List<PlayerDefaultSetting> DefaultPlayers => _defaultPlayers;
 
-    [SerializeField,
-     Tooltip(
-         "List of the player colors, OnPlayerJoin the respective color will be loaded\n(Ex. Player0 = ListElement0)")]
-    private List<Color> playerColors = new List<Color>();
+    
     
     private List<PlayerConfiguration> _playerConfigs;
     public List<PlayerConfiguration> PlayerConfigs => _playerConfigs;
+    
 
     public List<PlayerInputHandler> PlayerInputHandlers => FindObjectsByType<PlayerInputHandler>(FindObjectsSortMode.InstanceID).ToList();
 
@@ -37,6 +42,17 @@ public class PlayerConfigurationManager : MonoBehaviour
         }
         else
             Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        //TODO: check for errors
+        _defaultPlayers = new List<PlayerDefaultSetting>();
+        for (int i = 0; i < _playerColors.Count; i++)
+        {
+            _defaultPlayers.Add(new PlayerDefaultSetting(_playerColors[i]));
+            Debug.Log(_defaultPlayers[i].DefaultColor);
+        }
     }
 
     /// <summary>
@@ -64,7 +80,7 @@ public class PlayerConfigurationManager : MonoBehaviour
 
         if (_playerConfigs.All(p => p.PlayerIndex != input.playerIndex))
         {
-            _playerConfigs.Add(new PlayerConfiguration(input, playerColors[input.playerIndex]));
+            _playerConfigs.Add(new PlayerConfiguration(input));
             input.gameObject.transform.SetParent(transform);
         }
     }
@@ -73,6 +89,7 @@ public class PlayerConfigurationManager : MonoBehaviour
     {
         return _playerConfigs[playerIndex]; 
     }
+    
 }
 
 /// <summary>
@@ -86,11 +103,26 @@ public class PlayerConfiguration
     public string ControlScheme { get; set; }
     public bool IsReady { get; set; }
 
-    public PlayerConfiguration(PlayerInput input, Color color)
+    public PlayerConfiguration(PlayerInput input)
     {
         Input = input;
         PlayerIndex = input.playerIndex;
         ControlScheme = input.currentControlScheme;
+    }
+
+    public void SetColor(Color color)
+    {
         PlayerColor = color;
     }
+}
+
+public class PlayerDefaultSetting
+{
+    public Color DefaultColor;
+
+    public PlayerDefaultSetting(Color playerColor)
+    {
+        DefaultColor = playerColor;
+    }
+    //weapon
 }
--- Assets/Scripts/Multiplayer/PlayerSetupController.cs
+++ Assets/Scripts/Multiplayer/PlayerSetupController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -19,37 +19,90 @@ public class PlayerSetupController : MonoBehaviour
     [SerializeField]
     private Button readyButton;
 
+    [SerializeField] private List<Button> colorButtons;
+
     private PlayerInput _playerInput;
     private int _playerIndex;
-    private InputAction _inputAction;
-
-    private void OnDisable()
-    {
-        _inputAction.performed -= OnSubmit;
-        _inputAction.Disable();
-    }
+    private InputAction _submitAction;
+    private InputAction _navigationAction;
+    private int _currentColorID;
+    private Color _colorSelected; 
     
-    private void OnSubmit(InputAction.CallbackContext obj)
-    {
-        ReadyPlayer();
-    }
-
     public void SetPlayerInput(PlayerInput playerInput)
     {
         _playerInput = playerInput;
         
-        _inputAction = _playerInput.actions.FindActionMap("UI").FindAction("Submit");
-        _inputAction.performed += OnSubmit;
-        _inputAction.Enable();
+        _submitAction = _playerInput.actions.FindActionMap("UI").FindAction("Submit");
+        _submitAction.performed += OnSubmit;
+        _submitAction.Enable();
         
+        _navigationAction = _playerInput.actions.FindActionMap("UI").FindAction("Navigate");
+        _navigationAction.performed += OnNavigate;
+        _navigationAction.Enable();
+        
+        _colorSelected = Color.white;
         SetPlayerTexts();
-        SetSetupCardColor();
+        SetupColors();
+    }
+    
+    private void OnDisable()
+    {
+        _submitAction.performed -= OnSubmit;
+        _submitAction.Disable();
+        
+        _navigationAction.performed -= OnNavigate;
+        _navigationAction.Disable();
+    }
+    
+    private void OnSubmit(InputAction.CallbackContext obj)
+    {
+        if (_colorSelected == Color.white)
+        {
+            _colorSelected = colorButtons[_currentColorID].colors.normalColor;
+            var config = PlayerConfigurationManager.Instance.GetPlayerConfig(_playerIndex);
+            config.SetColor(_colorSelected);
+        }
+        else
+            ReadyPlayer();
     }
 
-    private void SetSetupCardColor()
+    private void OnNavigate(InputAction.CallbackContext obj)
     {
-        PlayerConfiguration playerConfig = PlayerConfigurationManager.Instance.GetPlayerConfig(_playerIndex);
-        GetComponent<Image>().color = playerConfig.PlayerColor;
+        if (!obj.performed || colorButtons.Count == 0) return;
+        
+        Vector2 inputValue = obj.ReadValue<Vector2>();
+        if (inputValue.x > 0)
+        {
+            _currentColorID = (_currentColorID + 1) % colorButtons.Count;
+        }
+        else if (inputValue.x < 0)
+        {
+            _currentColorID = (_currentColorID - 1 + colorButtons.Count) % colorButtons.Count;
+        }
+        
+        colorButtons[_currentColorID].Select();
+    }
+
+    private void SetupColors()
+    {
+        List<PlayerDefaultSetting> defaultPlayers = PlayerConfigurationManager.Instance.DefaultPlayers;
+
+        for (int i = 0; i < colorButtons.Count; i++)
+        {
+            Debug.Log(colorButtons[i].name);
+            Debug.Log(defaultPlayers[i]);
+            ChangeButtonColor(colorButtons[i], defaultPlayers[i].DefaultColor);
+        }
+    }
+    
+    private void ChangeButtonColor(Button button, Color newColor)
+    {
+        ColorBlock cb = button.colors;
+        cb.normalColor = newColor;
+        cb.highlightedColor = newColor;
+        cb.pressedColor = Color.white;
+        cb.selectedColor = new Color(newColor.r, newColor.g, newColor.b, 0.5f);
+        button.colors = cb;
     }
 
     private void SetPlayerTexts()
